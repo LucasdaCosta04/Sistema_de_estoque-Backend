176141dc13200e3fd8e709ba5091722841e843c6
Lucas da Costa <164957384+LucasdaCosta04@users.noreply.github.com>
2025-11-05 13:41:43 -0300

    Revert "Dev lucas"

--- src/main/java/estoque/server/ServidorMain.java
+++ src/main/java/estoque/server/ServidorMain.java
@@ -1,14 +1,4 @@
 package estoque.server;
 
-import estoque.dao.ConnectionFactory;
-
 public class ServidorMain {
-    public static void main(String[] args) {
-        try {
-            var conn = ConnectionFactory.getConnection();
-            System.out.println("✅ Conexão funcionando!");
-        } catch (Exception e) {
-            System.out.println("❌ Erro: " + e.getMessage());
-        }
-    }
 }
