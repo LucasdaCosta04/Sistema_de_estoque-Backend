b81c779e874a296b2145c9c4d2bb7e9e1fc5b2f9
LucasdaCosta04 <164957384+LucasdaCosta04@users.noreply.github.com>
2025-11-05 14:40:34 -0300

    feat: implementar modelo Produto e CRUD completo no ProdutoDAO - adicionado model Produto - criado ProdutoDAO com métodos inserir, listar, atualizar e excluir

--- src/main/java/estoque/model/Produto.java
+++ src/main/java/estoque/model/Produto.java
@@ -1,4 +1,96 @@
 package estoque.model;
 
 public class Produto {
+
+    private int id;
+    private String nome;
+    private double precoUnitario;
+    private String unidade;
+    private int quantidadeEstoque;
+    private int quantidadeMinima;
+    private int quantidadeMaxima;
+    private String categoria;
+
+    // Construtor vazio (obrigatório para frameworks e DAO)
+    public Produto() {
+    }
+
+    // Construtor completo
+    public Produto(int id, String nome, double precoUnitario, String unidade,
+                   int quantidadeEstoque, int quantidadeMinima, int quantidadeMaxima,
+                   String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.unidade = unidade;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.quantidadeMinima = quantidadeMinima;
+        this.quantidadeMaxima = quantidadeMaxima;
+        this.categoria = categoria;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public int getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public void setQuantidadeMaxima(int quantidadeMaxima) {
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
