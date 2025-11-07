1598781f2a67a10ad82bf234459684a1b3b90eec
LucasdaCosta04 <164957384+LucasdaCosta04@users.noreply.github.com>
2025-11-07 20:45:38 -0300

    refactor: move Produto from model to entity package

--- src/main/java/com/sistema/estoque/controller/ProdutoController.java
+++ src/main/java/com/sistema/estoque/controller/ProdutoController.java
@@ -1,6 +1,6 @@
 package com.sistema.estoque.controller;
 
-import com.sistema.estoque.model.Produto;
+import com.sistema.estoque.entity.Produto;
 import com.sistema.estoque.service.ProdutoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
