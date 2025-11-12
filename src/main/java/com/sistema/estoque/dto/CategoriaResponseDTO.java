de9afc2b506d278c8e8a63d5e8a0ccaf87ae99e2
Muniz2504 <104372006+Muniz2504@users.noreply.github.com>
2025-11-12 19:55:11 -0300

    Feat: adicionado construtores @NoArgsConstructor, @AllArgsConstructor e métodos getter e setter via lombok

--- src/main/java/com/sistema/estoque/dto/CategoriaResponseDTO.java
+++ src/main/java/com/sistema/estoque/dto/CategoriaResponseDTO.java
@@ -1,8 +1,18 @@
 package com.sistema.estoque.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoriaResponseDTO {
+
     private Long id;
     private String nome;
     private String descricao;
-    // getters/setters e construtor
+
 }
