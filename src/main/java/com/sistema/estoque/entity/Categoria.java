f5ffe7b2bb7ac180a1d5d3dee3465c6f41d32550
Muniz2504 <104372006+Muniz2504@users.noreply.github.com>
2025-11-12 19:52:31 -0300

    Refactor(Categoria): Categoria alterada para package entity

---
+++ src/main/java/com/sistema/estoque/entity/Categoria.java
@@ -0,0 +1,45 @@
+package com.sistema.estoque.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categorias", uniqueConstraints = @UniqueConstraint(columnNames = "nome"))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 100, unique = true)
+    private String nome;
+
+    @Column(length = 250)
+    private String descricao;
+
+    @Column(nullable = false)
+    private Instant criadoEm = Instant.now();
+
+
+    @OneToMany(
+            mappedBy = "categoria",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    private Set<Produto> produtos = new HashSet<>();
+
+
+
+}
