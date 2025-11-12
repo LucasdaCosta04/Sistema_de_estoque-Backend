a078312b1097b7a7d3779fa87f4847ab8c7d7f4f
Muniz2504 <104372006+Muniz2504@users.noreply.github.com>
2025-11-12 19:54:23 -0300

    Feat: adicionado métodos getter e setter via lombok

--- src/main/java/com/sistema/estoque/dto/CategoriaCreateDTO.java
+++ src/main/java/com/sistema/estoque/dto/CategoriaCreateDTO.java
@@ -2,13 +2,17 @@ package com.sistema.estoque.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CategoriaCreateDTO {
-    @NotBlank @Size(min = 2, max = 100)
+
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
     private String nome;
 
-    @Size(max = 250)
+    @Size(max = 250, message = "A descrição não pode exceder 250 caracteres")
     private String descricao;
-
-    // getters/setters
 }
