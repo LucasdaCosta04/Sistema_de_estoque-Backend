326f0a9d4c8f3d2a9113481d035bb7b500b2cdee
Muniz2504 <104372006+Muniz2504@users.noreply.github.com>
2025-11-16 13:20:06 -0300

    Fix: Modificado código com base no que já estava feito

--- src/main/java/com/sistema/estoque/dto/CategoriaCreateDTO.java
+++ src/main/java/com/sistema/estoque/dto/CategoriaCreateDTO.java
@@ -1,18 +1,14 @@
 package com.sistema.estoque.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Getter @Setter
 public class CategoriaCreateDTO {
 
-    @NotBlank(message = "O nome não pode estar em branco")
-    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+    @NotBlank(message = "O nome da categoria é obrigatório.")
     private String nome;
 
-    @Size(max = 250, message = "A descrição não pode exceder 250 caracteres")
     private String descricao;
 }
