dd47ad9a9855fb7da2e6695c8d5e766ba8b17842
Muniz2504 <104372006+Muniz2504@users.noreply.github.com>
2025-11-16 13:20:46 -0300

    Fix: Modificado código com base no que já estava feito

--- src/main/java/com/sistema/estoque/dto/CategoriaResponseDTO.java
+++ src/main/java/com/sistema/estoque/dto/CategoriaResponseDTO.java
@@ -2,17 +2,13 @@ package com.sistema.estoque.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class CategoriaResponseDTO {
-
     private Long id;
     private String nome;
     private String descricao;
-
 }
